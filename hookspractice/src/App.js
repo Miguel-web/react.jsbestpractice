f2fafd9401817e3f0f685652c5345f8b00297207
Miguel-web <miguelgilf@gmail.com>
2023-03-07 20:08:28 +0000

    primer ejercicio react js hooks uso del useState  nivel basico

--- hookspractice/src/App.js
+++ hookspractice/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import React, {use} from 'react';
+import React, {useState} from 'react';
 
-function App() {
+// ejercicio 1
+
+/*function App() {
+  
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
-      
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
+    
   );
-}
+}*/
 
+function App() {
+  
+  const [counter, setCounter] = useState(0)
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+    </div>
+    
+  );
+  }
+  
 export default App;
